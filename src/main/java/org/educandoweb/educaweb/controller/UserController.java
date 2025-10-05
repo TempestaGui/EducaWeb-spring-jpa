c0bc034756fa2bcb6e6b0af7c433718be6925f80
tempestaGui <guilhermeftempesta@gmail.com>
2025-10-05 17:39:22 -0300

    created User service and att user controller

--- src/main/java/org/educandoweb/educaweb/controller/UserController.java
+++ src/main/java/org/educandoweb/educaweb/controller/UserController.java
@@ -1,6 +1,8 @@
 package org.educandoweb.educaweb.controller;
 
 
+import org.educandoweb.educaweb.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,15 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import org.educandoweb.educaweb.entity.User;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/Users")
 public class UserController {
 
-    @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L,"teste","teste@gmail","123","senha123");
+    @Autowired
+    private UserService userService;
 
-        return ResponseEntity.ok().body(u);
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = userService.findAll();
+
+        return ResponseEntity.ok().body(list);
     }
 
 }
---
+++ src/main/java/org/educandoweb/educaweb/service/UserService.java
@@ -0,0 +1,23 @@
+package org.educandoweb.educaweb.service;
+
+import org.educandoweb.educaweb.entity.User;
+import org.educandoweb.educaweb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id){
+        return userRepository.findById(id).get();
+    }
+}
