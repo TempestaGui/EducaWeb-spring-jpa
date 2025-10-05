d56c0cb851830b2a4db5ae2ec0c7a0b7ef7a5e43
tempestaGui <guilhermeftempesta@gmail.com>
2025-10-05 18:01:04 -0300

    Implement User findById

--- src/main/java/org/educandoweb/educaweb/service/UserService.java
+++ src/main/java/org/educandoweb/educaweb/service/UserService.java
@@ -18,6 +18,6 @@ public class UserService {
     }
 
     public User findById(Long id){
-        return userRepository.findById(id).get();
+        return userRepository.findById(id).orElse(null);
     }
 }
